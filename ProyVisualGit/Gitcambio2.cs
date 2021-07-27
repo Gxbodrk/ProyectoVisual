23a67c6d4ce795045622d82b4b2e9e764d6ce019
GEOVANNY GABRIEL PILLAJO GONZALEZ <geovanny.pillajo.gonzalez@estudiantes.cordillera.edu.ec>
2021-07-27 10:35:12 -0500

    formulario2

---
+++ ProyVisualGit/Gitcambio2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyVisualGit
+{
+    public partial class Gitcambio2 : Form
+    {
+        public Gitcambio2()
+        {
+            InitializeComponent();
+        }
+    }
+}
